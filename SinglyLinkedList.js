d76bf9d522945e3118a92b2bfa28b120d7aa0a1b
Manish Bhardwaj <manishbhardwaj131149@gmail.com>
2022-11-02 23:59:03 +0530

    Created Class of linkedlist

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -5,3 +5,11 @@ class Node{
         this.next = null;
     }
 }
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}

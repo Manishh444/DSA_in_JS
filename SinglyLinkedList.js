f762e86f366da1504a03cc96fb9683fdd41ed6b2
Manish Bhardwaj <manishbhardwaj131149@gmail.com>
2022-11-03 13:19:00 +0530

    Push implemented

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -12,4 +12,17 @@ class SinglyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+    push(val){
+        this.newNode = val;
+        if(!this.head){
+            this.head = this.newNode;
+            this.tail = this.head
+        }
+        else{
+            this.tail.next = this.newNode;
+            this.tail = this.newNode
+        }
+        this.length++
+        return this;
+    }
 }

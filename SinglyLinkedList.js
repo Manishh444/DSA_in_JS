339782787d75e526120d5f0297eecdf4a975edd0
Manish Bhardwaj <manishbhardwaj131149@gmail.com>
2022-11-04 23:51:37 +0530

    dummy commit

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -16,14 +16,14 @@ class SinglyLinkedList{
         // create a node
         this.newNode = new Node(val);
         // check if list is empty 
-        if(!this.head){
+        if(!this.head){//if(not null)
             this.head = this.newNode;
-            this.tail = this.head
+            this.tail = this.newNode;
         }
         // else modify current tail property and set new tail
         else{
             this.tail.next = this.newNode;
-            this.tail = this.newNode
+            this.tail = this.newNode;
         }
         // increase length by one everytime push method is called and executed
         this.length++;
@@ -31,8 +31,12 @@ class SinglyLinkedList{
     }
 }
 let newList = new SinglyLinkedList();
-// console.log(newList.push("kuch"))
-newList.push("kuch")
-newList.push("such")
-newList.push("much")
 console.log(newList)
+newList.push(null)
+newList.push("such")
+newList.push("touch")
+newList.push("much")
+newList.push("lavender")
+console.log(newList)
+// console.log(newList.head.next.next)
+// console.log(newList.tail)

de9fe631f5a870d7ef24abe3c19905145a1865b3
Manish Bhardwaj <manishbhardwaj131149@gmail.com>
2022-11-05 23:49:34 +0530

    two pointer approach of linkedlist

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -14,16 +14,19 @@ class SinglyLinkedList{
     }
     push(val){
         // create a node
-        this.newNode = new Node(val);
+        this.newNode = new Node(val);// NODE {val: 2; next: null}
         // check if list is empty 
         if(!this.head){//if(not null)
             this.head = this.newNode;
-            this.tail = this.newNode;
+            this.tail = this.head;
         }
         // else modify current tail property and set new tail
         else{
             this.tail.next = this.newNode;
+            // console.log("this is tail.next",this.tail.next);
             this.tail = this.newNode;
+            // console.log("this is tail",this.tail);
+
         }
         // increase length by one everytime push method is called and executed
         this.length++;
@@ -31,12 +34,14 @@ class SinglyLinkedList{
     }
 }
 let newList = new SinglyLinkedList();
-console.log(newList)
-newList.push(null)
+// console.log(newList)
+// newList.push(null)
 newList.push("such")
 newList.push("touch")
-newList.push("much")
-newList.push("lavender")
-console.log(newList)
+// newList.push("much")
+
+// newList.push("lavender")
+// console.log(newList.head)
 // console.log(newList.head.next.next)
 // console.log(newList.tail)
+console.log(newList)

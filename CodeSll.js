d7cd3da5b50396e117b399078787b893296e55ec
Manish Bhardwaj <75626814+Manishh444@users.noreply.github.com>
2023-03-25 15:07:34 +0530

    Update CodeSll.js

--- CodeSll.js
+++ CodeSll.js
@@ -44,6 +44,18 @@ class SinglyLinkList{
     this.tail=null;}
     return temp;
   }
+  
+   shift(){
+    if(!this.head)return undefined;
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--
+    if(!this.length){
+      this.tail=null;
+    }
+    return temp
+  }
 }
 
 const newList = new SinglyLinkList();
